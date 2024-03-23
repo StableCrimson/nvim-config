75cbfef4603499fda2550b7037aefdccddc37b2f
rhyse302 <rhysethompson@gmail.com>
2024-03-23 18:46:12 -0500

    Add goto definition and code action

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,6 +36,8 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
       lspconfig.bashls.setup({})
       lspconfig.clangd.setup({})
       lspconfig.cssls.setup({})
